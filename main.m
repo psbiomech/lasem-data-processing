b97ab9ab7c0e2de3d16f232a508a24bc09be1e22
psbiomech <psbiomech@gmail.com>
2017-06-02 13:28:45 +1000

    pulls BB data from C3D

---
+++ main.m
@@ -0,0 +1,11 @@
+%% RUN BODYBUILDER DATA EXTRACTION
+% Prasanna Sritharan, June 2017
+
+
+c3dpath = 'C:\Users\psritharan\Documents\03 Projects\lasem-data-processing\Baseline\Sample Data';
+c3dfile = 'FAILT01_Walk01';
+
+
+
+
+bb = pullC3Ddata(c3dpath,c3dfile,[1 1 1]);
---
+++ pullC3Ddata.m
@@ -0,0 +1,73 @@
+function [bb,sflag] = pullC3Ddata(c3dpath,c3dfile,amp)
+
+%pullC3Ddata Get BodyBuilder data from C3D file
+%   Prasanna Sritharan, June 2017
+   
+    % Body Builder data groups
+    BBGROUPS = {'POWERS','ANGLES','MOMENTS'};
+
+    try
+
+        % add C3D extension if necessary
+        if isempty(regexpi(c3dfile,'.c3d')), c3dfile = [c3dfile '.c3d']; end;
+        
+        % initiate C3DServer
+        itf = c3dserver();
+        
+        % turn strict parameter checking off
+        itf.SetStrictParameterChecking(0);
+
+        % open C3D file
+        try
+            sflag = itf.Open([c3dpath '\' c3dfile],3);
+        catch
+            sflag = -1;
+            disp('ERROR: C3D file could not be opened for processing. Please check if file exists and is not corrupted.');
+        end
+        
+        % get first and last video frames
+        vfrange = [itf.GetVideoFrame(0) itf.GetVideoFrame(1)];        
+        
+        % determine data vector length (no. of video frames)
+        nframes = vfrange(2)-vfrange(1)+1;
+        
+        % get number of video channels used
+        idx = itf.GetParameterIndex('POINT','USED');
+        nused = itf.GetParameterValue(idx,0);               
+        
+        % get video channel list
+        vlist = cell(1,nused);
+        for n=1:nused
+            idx = itf.GetParameterIndex('POINT','LABELS');
+            vlist{n} = itf.GetParameterValue(idx,n-1);
+        end
+        
+        % get desired output data groups
+        outgrps = BBGROUPS(logical(amp));
+        
+        % get data for all parameters in each desired group and store in struct
+        for g=1:length(outgrps)
+            idx = itf.GetParameterIndex('POINT',outgrps{g});
+            nvals = itf.GetParameterLength(idx);
+            for n=0:nvals-1
+                qname = itf.GetParameterValue(idx,n);
+                vchan = find(strcmp(qname,vlist))-1;
+                bb.(outgrps{g}).(qname) = zeros(nframes,3);    % initialise Body Builder output data matrix
+                for x=1:3
+                   bb.(outgrps{g}).(qname)(:,x) = double(cell2mat(itf.GetPointDataEx(vchan,x-1,vfrange(1),vfrange(2),'1')));
+                end
+            end            
+        end                   
+        
+        
+        % close C3D file
+        itf.Close();
+        
+    catch exception
+        rethrow(exception)
+        sflag = -2;
+        disp('ERROR: C3D file could not be processed.');
+    end
+
+end
+
