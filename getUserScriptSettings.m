d66955e07e16bd1883a1f9c7a79329488f26ab72
psbiomech <psbiomech@gmail.com>
2018-05-31 18:41:25 +1000

    included output all trials to XLS

--- getUserScriptSettings.m
+++ getUserScriptSettings.m
@@ -33,7 +33,7 @@ user.SAMP = 100;    % resample data to standardised number of time steps
 user.AMPG = [1 1 1 1];   % what Body Builder data to extract from C3D file: angles, moments, powers, GRFs (1=yes,0=no)
 user.FILESELECTMODE = 'auto';   % 'auto': process C3D files matching file name format, 'manual': manually select which files to process
 user.TASKTYPE = 'sldj';   % activity/task/motion type code
-user.UPDATEMETA = 'noupdate';  % update subject cohort and affected limb via XLS (update/noupdate)
+user.UPDATEMETA = 'update';  % update subject cohort and affected limb via XLS (update/noupdate)
 
 % C3D file name parameters
 % (form: [SUBJECTPREFIX][2-digit numeric][SEPARATOR][TRIALPREFIX][2-digit numeric].c3d)
